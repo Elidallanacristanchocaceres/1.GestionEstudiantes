d582c6f0662e4b2e4db119b3e688cd26e27d0857
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2025-02-21 16:14:57 -0500

    feat: :sparkles: :Realizacion de cantidad de usuarios y solicitar nombre usuario

---
+++ tallerjava/src/main/java/com/tallerjava/Gestion.java
@@ -0,0 +1,27 @@
+package com.tallerjava;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Gestion {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+        
+      System.out.print("Ingrese la cantidad de estudiantes: ");
+      int cantidad = scanner.nextInt();
+      scanner.nextLine(); 
+
+      List<String> estudiantes = new ArrayList<>();
+        
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print("Ingrese el nombre del estudiante " + (i + 1) + ": ");
+            String nombre = scanner.nextLine();
+            estudiantes.add(nombre);
+        }
+
+
+        
+      }
+}
