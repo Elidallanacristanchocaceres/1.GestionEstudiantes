d24d34eeda299e7e2deaabe5032f05b7c3f239c8
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2025-02-23 15:04:17 -0500

    feat: :sparkles: : Añadir cantidad de estudiantes con sus nombres y notas

---
+++ src/main/java/com/tallerjava/RegistroNotas.java
@@ -0,0 +1,34 @@
+package com.tallerjava;
+import java.util.*;
+
+public class RegistroNotas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US); // Asegura el uso del punto como decimal
+        Map<String, Double> notasEstudiantes = new HashMap<>();
+
+        // 1.cantidad 
+        System.out.print("Ingrese la cantidad de estudiantes: ");
+        int cantidad = scanner.nextInt();
+        scanner.nextLine(); 
+
+        // 2.nombres y notas
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print("Ingrese el nombre del estudiante " + (i + 1) + ": ");
+            String nombre = scanner.nextLine();
+
+            double nota;
+            while (true) {
+                try {
+                    System.out.print("Cual es la nota " + nombre + " (Puedes colocar decimal): ");
+                    nota = Double.parseDouble(scanner.nextLine().replace(",", ".")); // Permite usar , o .
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Ingrese un número válido.");
+                }
+            }
+
+            notasEstudiantes.put(nombre, nota);
+        }
+        
+    }
+}
